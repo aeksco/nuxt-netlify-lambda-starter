5bb6051d00e7a94bb823a8de9b386299d07d76dd
Alexander Schwartzberg <aeksco@gmail.com>
2019-05-04 21:33:06 -0400

    Cleaned up About page

--- pages/about.vue
+++ pages/about.vue
@@ -1,33 +1,19 @@
 <template>
   <section class="container">
-    <div class="row">
+    <div class="row mt-4">
       <div class="col-lg-12">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h1 class="display-3">About</h1>
+            <h4 class="display-4">About</h4>
           </div>
         </div>
 
         <div class="row py-2">
           <div class="col-lg-12 text-center">
-            <p class="lead">visual code scaffolding for the modern web</p>
+            <p class="lead">This is just a simple About page</p>
             <p class="lead">
-              Hand-crafted code scaffolding for the modern web
+              Use it as a template to build out some simple pages
             </p>
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <div class="col-lg-6 text-center">
-            <hr class="mt-2" />
-          </div>
-        </div>
-
-        <div class="row mt-2 mb-4 justify-content-center">
-          <div class="col-lg-4">
-            <b-button to="/items" size="lg" variant="primary" block>
-              Check out dynamic static content (wow!)
-            </b-button>
           </div>
         </div>
       </div>
